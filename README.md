4290c0a3bb7340d670b983772320ac48050f0a24
bbubbush <bbubbush@naver.com>
2017-12-15 17:20:51 +0900

    Day 2
    Read Me 미작성
    
    코드만 완료

--- README.md
+++ README.md
@@ -2,7 +2,34 @@
 Django를 활용한 웹페이지 만들기(with 정두식)
 
 Day 1 (17/12/14)
-~ templates 
+~ 템플릿으로 html 불러오기
+####<정리>
+
+####[ Django 설치하기 ]
+>Python이 먼저 설치되어 있어야 하고 pip가 깔려있어야 한다
+    pip install django
+
+####[ Django 프로젝트 만들기 ]
+1. Django 프로젝트 생성
+- Power Shell으로 프로젝트를 만들 위치로 이동 후 
+
+    django-admin startproject {project_name} 
+
+2. Django 서버 실행
+- Power Shell으로 프로젝트 안으로 이동(manage.py가 있는 위치)
+
+    python manage.py runserver port_number(생략시 8000)
+
+3. Django 서버 접속
+- 127.0.0.1 혹은 localhost:8000
+- It Worked! 화면이 나오면 성공
+
+4. Django 서버 중단
+- Power Shell에 ctrl + c 입력 시 서버 중지
+
+
+Day 2 (17/12/14)
+~ 여론조사 화면 구현
 
 
 https://programmers.co.kr/learn/courses/6
--- elections/views.py
+++ elections/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Candidate
+from .models import Candidate, Poll, Choice
+import datetime as dt
 
 # Create your views here.
 def index(resquest):
     candidates = Candidate.objects.all()
     context = {'candidates': candidates}
     return render(resquest, 'elections/index.html', context)
+
+def areas(resquest, area):
+    today = dt.datetime.now()
+    try:
+        poll = Poll.objects.get(area = area, start_date__lte = today, end_date__gte = today)
+        candidates = Candidate.objects.filter(area = area)
+
+    except:
+        poll = None
+        candidates = None
+    context = {'candidates' : candidates,
+     'area' : area,
+     'poll' : poll}
+    return render(resquest, 'elections/area.html', context)
